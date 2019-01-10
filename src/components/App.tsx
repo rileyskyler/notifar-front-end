5f4546754a5f331c4dbf2b013a00b4dd83b89605
Skyler Riley <riley.skyler@gmail.com>
2019-01-09 22:39:42 -0700

    image in tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,21 @@
+// declare module "*.jpg"
 import * as React from "react";
+const logo = require('./photo.jpg')
+
 
 export interface AppProps {
 
 }
 
 export class App extends React.Component<AppProps, {}> {
-    render() {
-        return <h1>TS React App</h1>;
+    render() : any {
+        return (
+            <div>
+                <h1>
+                    TS React App
+                </h1>
+                    <img src={logo} alt="Test" />
+            </div>
+        )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,20 @@ module.exports = {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
+            // { test: /\.jpg?$/, loader: "file-loader"},
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                  'file-loader',
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      bypassOnDebug: true, // webpack@1.x
+                      disable: true, // webpack@2.x and newer
+                    },
+                  },
+                ],
+              },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
